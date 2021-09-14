c13e73dfbcc9a57e566a737005ed119d4893ae2e
Christian Nagel <christian@christiannagel.com>
2021-09-14 17:46:07 +0200

    ienumerable

---
+++ asyncstreams/Enumerable/ADevice.cs
@@ -0,0 +1,18 @@
+﻿namespace Enumerable
+{
+    public class ADevice
+    {
+        public IEnumerable<SomeData> GetSomeData()
+        {
+            Random r = new();
+            for (int i = 0; i < 100; i++)
+            {
+                int x = r.Next(1, 200);
+                yield return new SomeData($"text {x}", x);
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    public record SomeData(string Text, int Number); 
+}
---
+++ asyncstreams/Enumerable/Program.cs
@@ -0,0 +1,17 @@
+﻿using Enumerable;
+
+ADevice dev = new();
+var enumerable = dev.GetSomeData();
+using var enumerator = enumerable.GetEnumerator();
+while (enumerator.MoveNext())
+{
+    var item = enumerator.Current;
+    Console.WriteLine($"{item.Number}, {item.Text}");
+}
+
+
+//foreach (var item in dev.GetSomeData())
+//{
+//    Console.WriteLine($"{item.Number}, {item.Text}");
+//}
+
