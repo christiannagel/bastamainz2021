3542b264634db19a7c6cef4e72b8cacc24cee52a
Christian Nagel <christian@christiannagel.com>
2021-09-15 17:15:40 +0200

    async enumerable

---
+++ asyncstreams/AsyncEnumerable/ADevice.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AsyncEnumerable
+{
+    public class ADevice
+    {
+        public async IAsyncEnumerable<SomeData> GetSomeDataAsync([EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            Random r = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                int x = r.Next(1, 200);
+                yield return new SomeData($"text {x}", x);
+                await Task.Delay(100, cancellationToken);
+            }
+        }
+    }
+
+    public record SomeData(string Text, int Number); 
+}
---
+++ asyncstreams/AsyncEnumerable/Program.cs
@@ -0,0 +1,32 @@
+﻿using AsyncEnumerable;
+
+ADevice dev = new();
+//var enumerable = dev.GetSomeDataAsync();
+//await using var enumerator = enumerable.GetAsyncEnumerator();
+//while (await enumerator.MoveNextAsync())
+//{
+//    var item = enumerator.Current;
+//    Console.WriteLine($"{item.Number}, {item.Text}");
+//}
+
+CancellationTokenSource cts = new(TimeSpan.FromSeconds(10));
+
+try
+{
+
+    await foreach (var item in dev.GetSomeDataAsync().WithCancellation(cts.Token))
+    {
+        Console.WriteLine($"{item.Number}, {item.Text}");
+    }
+
+
+    //foreach (var item in dev.GetSomeData())
+    //{
+    //    Console.WriteLine($"{item.Number}, {item.Text}");
+    //}
+
+}
+catch (OperationCanceledException ex)
+{
+    Console.WriteLine(ex.Message);
+}
