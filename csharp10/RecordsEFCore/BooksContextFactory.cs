aebc2052ffd7b5243355893f8de99f69176863a5
Christian Nagel <christian@christiannagel.com>
2021-09-20 16:19:10 +0200

    records with ef core

---
+++ csharp10/RecordsEFCore/BooksContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+class BooksContextFactory : IDesignTimeDbContextFactory<BooksContext>
+{
+    public BooksContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<BooksContext> optionsBuilder = new();
+        string connectionString = "server=(localdb)\\mssqllocaldb;database=BastaBooksSample;trusted_connection=true";
+        optionsBuilder.UseSqlServer(connectionString);
+        return new BooksContext(optionsBuilder.Options);   
+    }
+}
+
---
+++ csharp10/RecordsEFCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("BooksConnection");
+        services.AddSqlServer<BooksContext>(connectionString);
+        //services.AddDbContext<BooksContext>(options =>
+        //{
+        //    options.UseSqlServer(connectionString);
+        //});
+        services.AddScoped<Runner>();
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+var runner = scope.ServiceProvider.GetRequiredService<Runner>();
+
+Console.Write("apply migrations? [y|n]");
+string input = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
+if (input == "yes" || input == "y")
+{
+    await runner.ApplyMigrationsAsync();
+}
+else
+{
+    await runner.CreateTheDatabaseAsync();
+}
+
+await runner.AddBookAsync("sample book", "sample");
+await runner.AddBooksAsync();
+await runner.ReadBooksAsync();
+await runner.QueryBooksAsync();
+await runner.UpdateBookAsync();
+await runner.DeleteBooksAsync();
+await runner.DeleteDatabaseAsync();
