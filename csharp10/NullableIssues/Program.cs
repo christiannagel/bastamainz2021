b28c803842e6106e01ade0cc122b84f5bd3ff17e
Christian Nagel <christian@christiannagel.com>
2021-09-21 09:59:31 +0200

    nullable issues

---
+++ csharp10/NullableIssues/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+A a1 = new() { Test1 = "42" };
+
+string x1 = a1.Test1;  // might be null - but no warning!
+// string x2 = x1.ToUpper();
+// Console.WriteLine(x2);
+
+
+string[] data = new string[3];
+
+string s1 = data[0];
+
+Console.WriteLine(s1.ToLower());
+
+public struct A
+{
+    public string Test1;  // not declared to be nullable, can't be initialized
+
+}
+
+
+public class InitializeSample
+{
+    public InitializeSample()
+    {
+        InitTest();
+    }
+
+    [MemberNotNull(nameof(Test))]
+    private void InitTest()
+    {
+        Test = "init";
+    }
+
+    public string Test { get; set; }
+}
